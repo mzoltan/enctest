13771cb7a89a4cf92deef5f149570a9ea0f8ce84
zoltanm <mz.lista@gmail.com>
2020-11-16 12:18:26 +0100

    initial commit

---
+++ src/main/java/com/example/encryptiontest/EncryptiontestApplication.java
@@ -0,0 +1,13 @@
+package com.example.encryptiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptiontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptiontestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/encryptiontest/EncryptiontestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encryptiontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptiontestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
